71d37b693a0edb3297f7e5d9fd5d037220de1939
Vinay Gunnam <vinaygunnam@live.com>
2015-03-16 23:14:47 -0500

    Only check in source files; Updated the folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,94 @@ var gulp        = require("gulp")
     babel       = require("gulp-babel"),
     transform   = require('vinyl-transform'),
     browserify  = require("browserify"),
+    watchify    = require("watchify"),
     connect     = require("gulp-connect"),
     watch       = require("gulp-watch"),
     ghPages     = require("gulp-gh-pages");
 
-gulp.task("default", ["webserver", "bundle"]);
+gulp.task("default", ["webserver", "copy"]);
 
-gulp.task("watch", ["default"], function() {
-    watch("apps/src/**/*.jsx", function() {
-        gulp.start("bundle");
-    });
-    
-    watch(["**/*.html", "apps/build/**/App.js"], function() {
+gulp.task("watch", ["default", "copy-watch"], function() {
+    watch(["dist/**/*.*"], function() {
         gulp.start("reload");
     });
 });
 
-gulp.task("jsx", function() {
-    return gulp.src("apps/src/**/*.jsx")
-                .pipe(react({harmony: true, es6module: true}))
-                .pipe(babel())
-                .pipe(gulp.dest("apps/src"));
-})
+gulp.task("jsx", jsx(false));
+gulp.task("jsx-watch", jsx(true));
 
-gulp.task("bundle", ["jsx"], function() {
-    // transform regular node stream to gulp (buffered vinyl) stream 
-    var browserified = transform(function(filename) {
-        var b = browserify({entries: filename, debug: true});
-        return b.bundle();
-    });
-    
-    return gulp.src("apps/src/**/App.js")
+function jsx(shouldWatch) {
+    return function() {
+        return prepareStream("apps/**/*.jsx", shouldWatch)
+                    .pipe(react({harmony: true, es6module: true}))
+                    .pipe(babel())
+                    .pipe(gulp.dest("apps"));
+    };
+}
+
+gulp.task("copy", ["copy-html", "copy-js", "copy-assets"]);
+gulp.task("copy-watch", ["copy-js-watch", "copy-html-watch", "copy-assets-watch"]);
+
+gulp.task("copy-js", ["jsx"], copyJs(false));
+gulp.task("copy-js-watch", ["jsx-watch"], copyJs(true));
+
+function copyJs(shouldWatch) {
+    return function() {
+        // transform regular node stream to gulp (buffered vinyl) stream
+        var browserified = transform(function(filename) {
+            var b = browserify({entries: filename, debug: true});
+            if (shouldWatch) {
+                b = watchify(b);
+            }
+            return b.bundle();
+        });
+        
+        return prepareStream("apps/**/App.js", false)
                 .pipe(browserified)
-                .pipe(gulp.dest("apps/build"));
-});
+                .pipe(gulp.dest("dist"));
+    }
+}
+
+gulp.task("copy-html", copyHtml(false));
+gulp.task("copy-html-watch", copyHtml(true));
+
+function copyHtml(shouldWatch) {
+    return function() {
+        return prepareStream(["*.html","apps/**/*.html"], shouldWatch)
+                .pipe(gulp.dest("dist"));
+    }
+}
+
+gulp.task("copy-assets", copyAssets(false));
+gulp.task("copy-assets-watch", copyAssets(true));
+
+function copyAssets(shouldWatch) {
+    return function() {
+        prepareStream("css/**/*.*", shouldWatch)
+                .pipe(gulp.dest("dist/css"));
+        prepareStream("fonts/**/*.*", shouldWatch)
+                .pipe(gulp.dest("dist/fonts"));
+        prepareStream("images/**/*.*", shouldWatch)
+                .pipe(gulp.dest("dist/images"));
+        prepareStream("js/**/*.*", shouldWatch)
+                .pipe(gulp.dest("dist/js"));
+    }
+}
+
+function prepareStream(source, shouldWatch, watchSource) {
+    var stream = gulp.src(source);
+    
+    if (shouldWatch) {
+        stream = stream.pipe(watch(watchSource || source));
+    }
+    
+    return stream;
+}
 
 gulp.task("webserver", function() {
     connect.server({
-        livereload: true
+        livereload: true,
+        root: "dist"
     });
 });
 
@@ -50,6 +100,6 @@ gulp.task("reload", function() {
 });
 
 gulp.task("deploy", function() {
-    return gulp.src(["apps/build/**/*", "*.html"])
+    return gulp.src("dist/**/*")
                 .pipe(ghPages());
 });
